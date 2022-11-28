167330c8238fa8bd25357974098d0edd69a46406
Devin Gundaniya <118975766+shradhdha-gundaniya@users.noreply.github.com>
2022-11-28 19:16:50 +0530

    123

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,10 @@ namespace Database\Seeders;
 
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
+//use Illuminate\Support\Facades\DB;
+//use App\Model\UserTableSeeder;
+//use Illuminate\Support\Str;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,21 +19,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $doe = DB::table('users')->insert([
-            'name' => 'Shraddha',
-            'email' => 'shraddha@laravel.test',
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-        ]);
-
-        $else = \App\Models\User::factory()->count(5)->create();
-
-        $users = $else->concat([$doe]);
         
-        $posts = App\Models\BlogPost::class->count(10)->create(); 
-            $post->user_id = $users->random()->id;
-            $post->save();
-    
+        $this->call(UsersTableSeeder::class);
+        
     }
 }
